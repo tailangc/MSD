791f2c2de0896c83db5f48370262d3b02d78d1b0
tailangc <tarou2001@outlook.com>
2023-08-21 12:08:35 -0600

    update gitignore file path

---
+++ Day1/HelloWorld/HelloWorld/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  HelloWorld
+//
+//  Created by Tailang  Cao on 8/21/23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!  \n";
+    return 0;
+}
