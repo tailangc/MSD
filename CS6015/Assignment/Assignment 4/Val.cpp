ca051efb0b9a4273e1b42ad76ac5e6c12e901520
tailangc <tarou2001@outlook.com>
2024-03-19 23:23:03 -0600

    6015 HW7,8

---
+++ CS6015/Assignment/Assignment 4/Val.cpp
@@ -0,0 +1,78 @@
+
+#include "Val.h"
+#include "Expr.h"
+
+#include <stdio.h>
+#include <iostream>
+
+
+NumVal::NumVal(int val){
+    this->val = val;
+}
+
+bool NumVal::equals(Val *other){
+    NumVal *other_num = dynamic_cast<NumVal*>(other);
+    if (other_num == NULL){
+        return false;
+    } else {
+        return this->val == other_num->val;
+    }
+}
+
+Expr* NumVal::to_expr(){
+    return new NumExpr(this->val);
+}
+
+Val *NumVal::add_to(Val *other){
+    NumVal *other_num = dynamic_cast<NumVal*>(other);
+    if (other_num == NULL) throw std::runtime_error("add of non-number");
+    return new NumVal(this->val + other_num->val);
+}
+
+Val *NumVal::mult_to(Val *other){
+    NumVal *other_num = dynamic_cast<NumVal*>(other);
+    if (other_num == NULL) throw std::runtime_error("multiplication of non-number");
+    return new NumVal(this->val * other_num->val);
+}
+
+void NumVal::print(std::ostream& outstream){
+    outstream << this->val;
+}
+
+bool NumVal::is_true(){
+    throw std::runtime_error("Test expression is not a boolean");
+}
+
+
+BoolVal::BoolVal(bool val){
+    this->val = val;
+}
+
+Expr* BoolVal::to_expr(){
+    return new BoolExpr(this->val);
+}
+
+bool BoolVal::equals(Val* other){
+    BoolVal *other_val = dynamic_cast<BoolVal*>(other);
+    if (other == NULL){
+        return false;
+    } else {
+        return (this->val == other_val->val);
+    }
+}
+
+Val* BoolVal::add_to(Val* rhs){
+    throw std::runtime_error("addition of non-number");
+}
+
+Val* BoolVal::mult_to(Val* rhs){
+    throw std::runtime_error("multiplication of non-number");
+}
+
+void BoolVal::print (std::ostream& outstream){
+    outstream << this->val;
+}
+
+bool BoolVal::is_true(){
+    return this->val;
+}
---
+++ CS6015/Assignment/Assignment 4/Val.h
@@ -0,0 +1,47 @@
+
+
+#ifndef ASSIGNMENT_4_VAL_H
+#define ASSIGNMENT_4_VAL_H
+
+#include <stdio.h>
+#include <iostream>
+
+class Expr;
+
+class Val{
+public:
+    virtual Expr* to_expr() = 0;
+    virtual bool equals(Val* v) = 0;
+    virtual Val* add_to(Val* rhs) = 0;
+    virtual Val* mult_to(Val* rhs) = 0;
+    virtual void print(std::ostream& outstream) = 0;
+    virtual bool is_true() = 0;
+};
+
+class NumVal: public Val {
+public:
+    int val;
+
+    NumVal(int val);
+    Expr* to_expr();
+    bool equals(Val* v);
+    Val* add_to(Val* rhs);
+    Val* mult_to(Val* rhs);
+    void print (std::ostream& outstream);
+    bool is_true();
+};
+
+class BoolVal: public Val {
+public:
+    bool val;
+
+    BoolVal(bool val);
+    Expr* to_expr();
+    bool equals(Val* v);
+    Val* add_to(Val* rhs);
+    Val* mult_to(Val* rhs);
+    void print (std::ostream& outstream);
+    bool is_true();
+};
+
+#endif /* val_hpp */
