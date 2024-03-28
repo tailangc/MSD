0ab7da4b54a536c39398dba37cf9a9d9dc32d005
tailangc <tarou2001@outlook.com>
2024-03-28 09:34:27 -0600

    Cryptography

--- CS 6014/Assignment/Cryptography/RC4.cpp
+++ CS 6014/Assignment/Cryptography/RC4.cpp
@@ -1,5 +1,26 @@
-//
-// Created by Tailang  Cao on 3/24/24.
-//
-
 #include "RC4.h"
+
+#include <utility>
+#include "blockCipher.h"
+
+RC4::RC4(std::string password) {
+    createKey(password, key);
+    for (int i = 0; i < 256; i++) { // identity permutation
+        S[i] = i;
+    }
+    j = 0;
+    for (int i = 0; i < 256; i++) { // identity permutation
+        j = (j + S[i] + key[i % password.length()]) % 256;
+        std::swap(S[i], S[j]);
+    }
+    i = 0;
+    j = 0;
+}
+
+uint8_t RC4::getNextByte() {
+    i = (i + 1) % 256;
+    j = (j + S[i]) % 256;
+    std::swap(S[i], S[j]);
+    int output = S[(S[i] + S[j]) % 256];
+    return S[output];
+}
--- CS 6014/Assignment/Cryptography/RC4.h
+++ CS 6014/Assignment/Cryptography/RC4.h
@@ -1,16 +1,20 @@
-//
-// Created by Tailang  Cao on 3/24/24.
-//
 
 #ifndef CRYPTOGRAPHY_RC4_H
 #define CRYPTOGRAPHY_RC4_H
 
 
+#include <array>
 
 class RC4 {
-
+public:
+    int i, j;
+    std::array<uint8_t, 8> key;
+    std::array<uint8_t, 256> S;
+public:
+    RC4();
+    RC4(std::string password);
+    uint8_t getNextByte();
 };
-
 
 
 #endif //CRYPTOGRAPHY_RC4_H
